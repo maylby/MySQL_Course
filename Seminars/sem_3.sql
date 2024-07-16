1c359279a6f037b0628a8e721f46501b149b6c6e
Vit <mayllby@gmail.com>
2024-07-16 16:04:49 +0300

    Добавил данные

--- Seminars/sem_3.sql
+++ Seminars/sem_3.sql
@@ -142,3 +142,14 @@ CREATE TABLE employee_tbl (
     work_date DATE,
     daily_typing_pages INT
 );
+
+-- Наполнение данными
+INSERT INTO employee_tbl (id, name, work_date, daily_typing_pages)
+VALUES
+(1, 'John', '2007-01-24', 250),
+(2, 'Ram', '2007-05-27', 220),
+(3, 'Jack', '2007-05-06', 170),
+(3, 'Jack', '2007-04-06', 100),
+(4, 'Jill', '2007-04-06', 220),
+(5, 'Zara', '2007-06-06', 300),
+(5, 'Zara', '2007-02-06', 350);
