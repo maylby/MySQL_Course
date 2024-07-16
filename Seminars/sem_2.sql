c0dc6e3bbaf11f374857a584496131a8f7b2be39
Vit <mayllby@gmail.com>
2024-07-16 15:09:48 +0300

    Добавил код

--- Seminars/sem_2.sql
+++ Seminars/sem_2.sql
@@ -15,3 +15,20 @@ storyline -- сюжетная линия, небольшое описание ф
 Все поля (кроме title_eng, count_min и storyline) обязательны для заполнения. 
 Поле id : первичный ключ, который заполняется автоматически.
 */
+
+USE lesson_1;
+CREATE TABLE movies
+(
+	-- id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY, -- SERIAL = BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE
+					|	|	|	|		|_-- Уникальный (???)
+					|	|	|	|_-- Авто присвоение id
+					|	|	|_-- Не нулевой
+					|	|_-- Брать, только положительную часть "BIGINT"
+					|_-- Расширенный "integer" (64 бита, от -9 до 9 млрд.)
+    title VARCHAR(60) NOT NULL,
+    title_eng VARCHAR(60),
+    year_movie YEAR NOT NULL,
+    count_min INT,
+    storyline TEXT
+);
