82dc573782e245348d26143e7c0d5dca0e4c2524
Vit <mayllby@gmail.com>
2024-07-17 17:04:40 +0300

    Добавил решение

--- Seminars/sem_5.sql
+++ Seminars/sem_5.sql
@@ -60,3 +60,14 @@ SELECT *
 FROM count_status
 WHERE status = 'active';
 
+
+WITH RECURSIVE sequence (n) AS
+(
+  SELECT 0
+  UNION ALL
+  SELECT n + 1 
+  FROM sequence 
+  WHERE n + 1 <= 10
+)
+SELECT n
+FROM sequence;
