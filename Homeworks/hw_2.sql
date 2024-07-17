2dfac03e975da6ac86a04ca6759e99870f4a1eb3
Vit <mayllby@gmail.com>
2024-07-17 17:42:19 +0300

    Добавил условие

--- Homeworks/hw_2.sql
+++ Homeworks/hw_2.sql
@@ -3,3 +3,23 @@
 Урок 2. SQL – создание объектов, простые запросы выборки
 https://gb.ru/lessons/441876/homework
 */
+
+-- Условие:
+/*
+Имеется таблица (сущность) с мобильными телефонами mobile_phones.
+У сущности имеются следующие поля(атрибуты):
+id – идентификатор;
+product_name – название;
+manufacturer – производитель;
+product_count – количество;
+price – цена.
+
+Сущность mobile_phones имеет следующие записи:
+| id | product_name | manufacturer | product_count | price | 
+| 1 | iPhone X | Apple | 156 | 76000 | 
+| 2 | iPhone 8 | Apple | 180 | 51000 | 
+| 3 | Galaxy S9 | Samsung | 21 | 56000 | 
+| 4 | Galaxy S8 | Samsung | 124 | 41000 | 
+| 5 | P20 Pro | Huawei | 341 | 36000 |
+
+*/
