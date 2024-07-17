feeb4f6ddedc140d327a8ec8269f74904596c70e
Vit <mayllby@gmail.com>
2024-07-17 16:14:05 +0300

    Добавил код

--- Seminars/sem_4.sql
+++ Seminars/sem_4.sql
@@ -21,3 +21,24 @@ SELECT *
 FROM tableA 
 INNER JOIN tableB
 ON tableA.name = tableB.name;
+
+
+CREATE TABLE members
+(
+member_id INT AUTO_INCREMENT,
+name VARCHAR (100) ,
+PRIMARY KEY (member_id)
+);
+
+CREATE TABLE committees
+(
+committee_id INT AUTO_INCREMENT,
+name VARCHAR (100),
+PRIMARY KEY (committee_id)
+);
+
+INSERT INTO members (name)
+VALUES("John"), ("Jane"), ("wary"), ("David"), ("2melia");
+INSERT INTO committees (name)
+VALUES ("John"), ("Mary"), ("Amelial"), ("Joe");
+SELECT * FROM members;
