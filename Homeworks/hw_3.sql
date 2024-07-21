3d54e330de14cf6c19b10188201f46e670715355
Vit <mayllby@gmail.com>
2024-07-21 21:25:04 +0300

    Добавил код

--- Homeworks/hw_3.sql
+++ Homeworks/hw_3.sql
@@ -17,3 +17,17 @@ salary – заработная плата;
 age - возраст.
 */
 
+DROP TABLE IF EXISTS staff; -- Удаление существующей таблицы
+-- CREATE TABLE staff -- Команда выдаст ошибку, если таблица уже существует
+CREATE TABLE IF NOT EXISTS staff /* Создание таблицы при уже существующей, 
+									не прерывая работу кода */
+(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	firstname VARCHAR(45),
+	lastname VARCHAR(45),
+	post VARCHAR(100),
+	seniority INT,
+	salary INT,
+	age INT
+);
+
